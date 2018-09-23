73949a75722e117df07569cc4205d66dc6bf8fd3
sateesh-rubyonrails <sateesh.rubyonrails@gmail.com>
2018-09-23 14:17:52 +0530

    millte types with description

---
+++ millet_types.rb
@@ -0,0 +1,53 @@
+1) # Proso Millet => Varigalu(Name in telugu language)
+Uses: -
+Another popular product from Richmillet called Proso Millet/Varigalu is highly recommended for Indian women as it is packed with 
+Calcium which helps in making the bones strong plus improves dental health. Proso Millet/Varigalu is also good for heart patients as it 
+reduces cholesterol. If you are into the glamour world then consuming Proso Millet will reduce ageing. For job going people who 
+handle lots of stress Proso Millet/Varigalu relaxes and strengthens the nervous system. Apart from this Proso Millet from Richmillet is a
+favorite for chefs/food lovers as it can be used in making bread, pasta, rich sweet dessert, even biryani, pulao or mixed rice in 
+both veg and non-veg recipes.
+
+2) # Kodo millet => Arikelu 
+Uses:-
+Kodo millet is very easy to digest commonly called as arikelu. In Tamil Nadu millet used as an alternative to rice called varagu rice. 
+It contains a high amount of lecithin and is excellent for strengthening the nervous system. Kodo millets are rich in B6 and folic acid, 
+especially niacin, B vitamins, as well as the minerals such as iron, calcium, potassium, zinc and magnesium. Arikelu does not contain any 
+gluten and is good for people who are gluten intolerant. Including kodo millet in your diet is beneficial to control high blood pressure 
+and high cholesterol levels. Kodo millet is a great way to get your proteins. Varagu is rich in fiber which is very good food for diabetic 
+patients. arikelu are low in fat and high in fiber keeps our tummy satiated for a very long time which makes the millet an ideal weight 
+loss food. The paste of Kodo Millet and water, when applied as a poultice on the wounds, accelerates wound healing.
+  
+3) # Little Millet => Samalu
+
+Uses:-
+Very high in nutrition content Little Millets from Richmillet are superior to rice, wheat, maize or pulses in terms of proteins, 
+minerals and vitamins. Little Millets are filled with calcium, iron, potassium, magnesium and zinc. Like all millets, Little Millets are 
+also gluten-free and has low-GI (Glycemic index) which is why these millets are suitable for people with wheat allergies or gluten 
+intolerance. Little Millets are a natural diet for diabetics and is excellent for people with uncontrollable body weight. 
+Little Millets are anti-constipation and encourages appetite. Known as Nutri cereals Little Millets are a wholesome diet for the whole 
+family.
+4) # Barnyard Millet => Udalu
+Barnyard Millet or Samvat ke Chawal or Vrat ke Chawal is from Richmillet. It is eaten after fasting as its name suggests. 
+Barnyard Millets are a healthy alternative to cereal grains like rice. These tiny round pale grains are wealthy in fiber, 
+calcium and phosphorus. When compared to wheat or maize Barnyard Millets are six times more fibrous and healthier. 
+They have almost equal protein quantity as Wheat or Maize. Lowest in carbohydrates when compared to other millets, 
+Barnyard Millets are a good source of vitamins especially B complex. Barnyard Millets are a medically recommended food for all 
+types of coeliac patients and patients with gluten intolerance. Also known as Sama ke Chawal.
+
+5) # Proso Millet => Varigalu 
+Another popular product from Richmillet called Proso Millet/Varigalu is highly recommended for Indian women as it is packed with 
+Calcium which helps in making the bones strong plus improves dental health. Proso Millet/Varigalu is also good for heart patients as it
+reduces cholesterol. If you are into the glamour world then consuming Proso Millet will reduce ageing. For job going people who handle 
+lots of stress Proso Millet/Varigalu relaxes and strengthens the nervous system. Apart from this Proso Millet from Richmillet is a 
+favorite for chefs/food lovers as it can be used in making bread, pasta, rich sweet dessert, even biryani, pulao or mixed rice in 
+both veg and non-veg recipes.
+  
+
+
+
+
+
+
+
+
+
